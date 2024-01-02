61ec266254a36d2ba46c3f6bcdb52a4d07d91ccc
Sebastian Weiß <weiss@nerd-labor.de>
2024-01-02 23:49:54 +0100

    clear nginx config before new apply

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -5,6 +5,7 @@ apt-get -y update
 apt-get -y install certbot python3-certbot-nginx
 
 # Insert domain into nginx
+echo "" > /etc/nginx/sites-enabled/default
 cp nginx_conf_template.txt /etc/nginx/conf.d/$1.conf
 sed -i "s/<DOMAIN>/\\$1/" /etc/nginx/conf.d/$1.conf
 nginx -t
