27db471ca390b9e4758b711699b6c79e30944e02
Sebastian Weiß <weiss@nerd-labor.de>
2024-01-02 23:39:37 +0100

    fix bugs

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Updating and installing dependencies
-apt-get update
-apt-get install certbot
-apt-get install python3-certbot-nginx
+apt-get -y update
+apt-get -y install certbot python3-certbot-nginx
 
 # Insert domain into nginx
 cp nginx_conf_template.txt /etc/nginx/conf.d/$1.conf
